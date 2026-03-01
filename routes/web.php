bfccae127bd6f193d1b3dc4090116dbce2c64559
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-03-01 10:24:39 +0700

    feat: inisiasi project Laravel – [Nabila Khustia rohmah]

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+ 
+
+Route::get('/', function () {
+
+    return view('welcome');
+
+});
+
+ 
+
+// Route baru – wajib ditambahkan!
+
+Route::get('/perkenalan', function () {
+
+    return '<h1>Halo! Nama saya Nabila Khustia Rohmah</h1>
+
+            <p>NIM: 4124021 | Prodi: Sistem Informasi</p>
+
+            <p>Saya siap belajar Laravel! 🚀</p>';
+
+});
