d1c6534e20cdd42139fecbfd7fd0d151d58dafe6
marth-22 <fauziyahmarthaaula322@gmail.com>
2026-03-06 16:30:03 +0700

    Menambahkan route Fauziah Martha di web.php

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
- 
-
 Route::get('/', function () {
-
     return view('welcome');
-
 });
 
- 
-
 // Route baru – wajib ditambahkan!
-
 Route::get('/perkenalan', function () {
-
     return '<h1>Halo! Nama saya Nabila Khustia Rohmah</h1>
-
             <p>NIM: 4124021 | Prodi: Sistem Informasi</p>
-
             <p>Saya siap belajar Laravel! 🚀</p>';
+});
 
+Route::get('/fauziahmartha', function () {
+    return '<h1>Halo! Nama saya Fauziah Martha Aula</h1>
+            <p>NIM: 4124006 | Prodi: Sistem Informasi</p>
+            <p>Saya siap belajar Laravel! 🚀</p>';
 });
