b579536964041dd61c70251f639c85f6f887cfa9
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-25 19:33:55 +0700

    2

--- api/index.php
+++ api/index.php
@@ -1,2 +1,19 @@
 <?php
+
+// Buat direktori yang dibutuhkan di /tmp (writable di Vercel)
+$directories = [
+    '/tmp/storage/app',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/views',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0775, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
