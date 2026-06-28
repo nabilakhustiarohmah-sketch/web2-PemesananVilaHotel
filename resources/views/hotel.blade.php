67753e535eb4c2891862210f9233023b1d17204c
nabila <nabilakhustiarohmah@saintek.unipdu.ac.id>
2026-06-28 12:19:56 +0700

    23

--- resources/views/hotel.blade.php
+++ resources/views/hotel.blade.php
@@ -15,7 +15,7 @@
     <div class="relative">
 
         <a href="{{ route('produk.show', $p->id) }}" class="block">
-            <img src="{{ $data->foto_utama }}"class="w-full h-full object-cover">
+            <img src="{{ $p->foto_utama }}"class="w-full h-full object-cover">
         </a>
 
         {{-- FAVORIT --}}
